785758c4fc04b63a3da06783b12c3992f87d6638
Dhileepkumar Ravichandran <91655068+Dhileep1234@users.noreply.github.com>
2024-12-25 23:22:38 +0530

    Add files via upload

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,27 +1,3 @@
-// const jwt = require('jsonwebtoken');
-
-// // Replace this value with your JWT secret key
-// const JWT_SECRET = '470ff8c244542723a8b69f4851a1d1b990ab904a16bdfd90f4134bb057e21eba';
-
-// const authenticateToken = (req, res, next) => {
-//   const authHeader = req.headers['authorization'];
-//   if (!authHeader) return res.sendStatus(401);
-
-//   const token = authHeader.split(' ')[1];
-//   if (!token) return res.sendStatus(401);
-
-//   jwt.verify(token, JWT_SECRET, (err, user) => {
-// //   jwt.verify(token.split(' ')[1], JWT_SECRET, (err, user) => {
-//     if (err) return res.sendStatus(403);
-//     // if (err) return (err);
-//     console.log(err);
-//     req.user = user;
-//     next();
-//   });
-// };
-
-// module.exports = authenticateToken;
-
 const jwt = require('jsonwebtoken');
 
 const JWT_SECRET="Jwt";
